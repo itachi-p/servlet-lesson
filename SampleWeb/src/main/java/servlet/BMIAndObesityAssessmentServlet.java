5976b7e25e2d2950412107df82c3f01e234de4e6
itachi-p <itachip38@gmail.com>
2024-11-06 15:59:04 +0900

    演習問題3&4

---
+++ SampleWeb/src/main/java/servlet/BMIAndObesityAssessmentServlet.java
@@ -0,0 +1,64 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/BMIAndObesity")
+public class BMIAndObesityAssessmentServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><head><title>BMI計算結果</title></head><body>");
+
+		// 数字以外が入力された場合のエラーをキャッチする例外処理
+		try {
+			double height = Double.parseDouble(request.getParameter("height"));
+			double weight = Double.parseDouble(request.getParameter("weight"));
+			// BMIの計算
+			int bmi = calculateBMI(height, weight);
+			// 肥満度を判定
+			String obesityLevel = assessObesityLevel(bmi);
+			
+			pw.println("あなたの身長は" + height + "cmです。<br />");
+			pw.println("あなたの体重は" + weight + "kgです。<br />");
+			pw.println("BMIは" + bmi + " 肥満度は" + obesityLevel + "です。");
+		} catch (NumberFormatException e) {
+			pw.println(e.getMessage() + "<br>数字を入力してください。");
+		}
+
+	} // doPost()
+	
+	private int calculateBMI(double height, double weight) {
+		height /= 100; // 身長をメートル単位に変換
+		int bmi = (int)(weight / (height * height));
+		return bmi;
+	}
+	
+	private String assessObesityLevel(int bmi) {
+		String obesityLevel;
+		if (bmi <= 18) {
+			obesityLevel = "低体重";
+		} else if (bmi < 25) {
+			obesityLevel = "標準";
+		} else if (bmi < 30) {
+			obesityLevel = "肥満度１";
+		} else if (bmi < 35) {
+			obesityLevel = "肥満度２";
+		} else if (bmi < 40) {
+			obesityLevel = "肥満度３";
+		} else {
+			obesityLevel = "肥満度４";
+		}
+		return obesityLevel;
+	}
+
+}
---
+++ SampleWeb/src/main/java/servlet/InputServlet.java
@@ -0,0 +1,70 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/InputServlet")
+public class InputServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		// TODO Auto-generated method stub
+		// リクエストの文字コードを指定(デフォルト値の"ISO-8859-1（Latin-1）"では日本語が文字化けする)
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+		// リクエストから値を取得
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		String koibito = request.getParameter("koibito");
+		String[] hobby;
+		// 趣味に1つもチェックが入ってない場合nullになるので、新たに配列を確保する
+		if (request.getParameterValues("hobby") != null) {
+			// 同じnameで複数の値が存在する場合はgetParameterValues()で配列に格納する
+			hobby = request.getParameterValues("hobby");
+		} else {
+			hobby = new String[1];
+			hobby[0] = "無趣味";
+		}
+		
+		PrintWriter pw = response.getWriter();
+		pw.println("<html><head><title>自己紹介ページ表示</title></head>");
+		pw.println("<body>");
+		pw. println("<p><font size=\"＋1\" color=\"red\">プロフィール</font></p>");
+		
+		if (name.length() > 0) {
+			pw.println("氏名は" + name +"です<br />");
+		} else {
+			pw.println("氏名は空白です");
+		}
+		pw.println("年齢は" + age + "歳です<br />");
+		
+		if (koibito.equals("0")) {
+			pw.println("恋人はいません<br />");
+		} else {
+			pw.println("恋人はいます<br />");
+		}
+		
+		pw.println("趣味は"); // printlnだと後に半角スペースが入る
+		int cnt = 0;
+		for (int i = 0; i < hobby.length; i++) {
+			if(hobby[i] != null) {
+				if(cnt >= 1) { // 複数選択されている場合は間に「と」を入れる処理
+					pw.print("と"); // 後ろに空白を開けない為にprintlnでなくprintを使う
+				}
+				pw.print(hobby[i]);
+				cnt++;
+			}
+		}
+		pw.println("です<br />");
+		pw.print("性格は" + request.getParameter("seikaku") + "です");
+		pw.println("<br><br><a href=\"/SampleWeb/html/profile.html\">戻る</a>");
+		pw.println("</body></html>");
+		pw.close();
+	}
+
+}

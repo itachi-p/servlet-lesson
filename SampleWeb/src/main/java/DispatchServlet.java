902b62541d61f0a4ee9650d0f57b98438bc34447
itachi-p <itachip38@gmail.com>
2024-11-09 21:49:38 +0900

    Servlet と JSP の連携初歩

---
+++ SampleWeb/src/main/java/DispatchServlet.java
@@ -0,0 +1,19 @@
+ import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DispatchServlet")
+public class DispatchServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException,IOException {
+		// 画面の遷移先を設定
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/jsp/dispatch.jsp");
+		// リクエストの転送(実際のレスポンスは転送先のJSPが返す)
+		dispatcher.forward(request, response);
+	}
+}
---
+++ SampleWeb/src/main/webapp/jsp/dispatch.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<html>
+<head>
+	<title>Dispatch</title>
+</head>
+<body>
+	<p>サーブレットから呼び出されたJSPです。</p>
+</body>
+</html>

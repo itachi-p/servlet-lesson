0ab2e5722a46b0a90a1e95c682c0881ddaa7e0a6
itachi-p <itachip38@gmail.com>
2024-11-26 17:29:20 +0900

    メイン画面を追加

---
+++ dokoTsubu/src/main/java/servlet/Main.java
@@ -0,0 +1,45 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.Mutter;
+import model.User;
+
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 全ユーザのつぶやきリストをアプリケーションスコープから取得
+		ServletContext application = this.getServletContext();
+		List<Mutter> mutterList = (List<Mutter>)application.getAttribute("mutterList");
+		// 取得できなかった場合(最初のアクセス時)は、新規作成しアプリケーションスコープに保存
+		if (mutterList == null) {
+			mutterList = new ArrayList();
+			application.setAttribute("mutterList", mutterList);
+		}
+		
+		// ログイン状態かを確認するためセッションスコープからユーザ情報を取得
+		HttpSession session = request.getSession();
+		User loginUser = (User)session.getAttribute("loginUser");
+		
+		if (loginUser == null) { // ログインしていない場合はリダイレクト
+			response.sendRedirect("index.jsp");
+		} else { // ログイン済みの場合はフォワード
+			RequestDispatcher dispatcher = request.getRequestDispatcher("WEB-INF/jsp/main.jsp");
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
